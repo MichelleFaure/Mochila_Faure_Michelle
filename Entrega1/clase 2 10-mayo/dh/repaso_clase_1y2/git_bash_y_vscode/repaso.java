e2fa703886069a969d1b60923acad8a7ed9af4ce
Michelle Faure <faure.s.michelle@gmail.com>
2023-05-23 21:29:13 -0400

    se agregan trabajos semana 1

---
+++ Entrega1/clase 2 10-mayo/dh/repaso_clase_1y2/git_bash_y_vscode/repaso.java
@@ -0,0 +1,8 @@
+Una terminal es una interfaz que nos sirve para comunicarnos con una computadora, 
+un teclado para entrada de datos y una pantalla para mostrar únicamente caracteres 
+alfanuméricos (sin gráficos).
+GitBash es una aplicacion de terminal que se utiliza como interfaz de sistema operativo 
+mediante comandos escritos (CLI)
+Visual Studio Code o VSCode es un IDE (entorno de desarrollo integrado) desarrollado por Microsoft. 
+Cuando hablamos de IDE nos referimos a un conjunto de herramientas diseñadas para facilitarnos la 
+creacion y el desarrollo de nuestros programas o aplicaciones.
---
+++ Entrega1/clase 2 10-mayo/dh/repaso_clase_1y2/interfaces_de_usuario/repaso.py
@@ -0,0 +1,3 @@
+Una interfaz de usuario es el medio a través del cual un usuario interactúa con un dispositivo, 
+aplicación o sistema. Es el punto de interacción entre el usuario y la tecnología, permitiéndole 
+realizar acciones, recibir información y comunicarse con el sistema de manera efectiva.
